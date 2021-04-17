ca7025019de3b9aea7936e3b19a14e768374e9cf
shafidark <safi29317@gmail.com>
2021-04-17 14:55:35 +0600

    after give the avatar

--- src/Components/Home/Topics/DoctorsName.js
+++ src/Components/Home/Topics/DoctorsName.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+
 
 const DoctorsName = (props) => {
     const {name, id, username} = props.data
    
     return (
         <div>
+            <AccountCircleIcon/>
            <p>{name}</p>
         </div>
     );
